ca0485114857436e37298b2a662974c3375ba983
Александр Sabbath <mymailbox099@gmail.com>
2023-11-15 07:49:41 +0200

    after all

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -4,19 +4,15 @@ const path = require('path');
 const { test, expect } = require('@playwright/test');
 require('dotenv').config();
 
+test("Login", async({page}) => {
+  console.log('ENV:', process.env.ENV);
+  console.log('USER:', process.env.USER);
+  console.log('PASS:', process.env.PASS);
 
-test("Login" , async({page}) => {
-console.log('ENV:', process.env.ENV);
-console.log('USER:', process.env.USER);
-console.log('PASS:', process.env.PASS);
-
-  // @ts-ignore
   await page.goto(process.env.ENV);
-  // @ts-ignore
   await page.fill('input[name="username"]', process.env.USER);
-  // @ts-ignore
   await page.fill('input[name="password"]', process.env.PASS);
-  await page.waitForTimeout(5000)
+  await page.waitForTimeout(5000);
 
   // Click on "Login"
   await page.click('button[type="submit"]');
@@ -26,35 +22,39 @@ console.log('PASS:', process.env.PASS);
     console.log('Login successful!');
   } catch (error) {
     console.error('Login failed within 2 seconds.');
-    throw error
+    throw error;
   }
-  // @ts-ignore
-  await page.context().storageState({path:"./aut.json"});
-  await page.waitForTimeout(10000)
 
-})
+  await page.context().storageState({ path: "./aut.json" });
+  await page.waitForTimeout(10000);
+});
 
-test("reOpen" , async({browser}) => {
-  
-  const context = await browser.newContext({storageState: "./aut.json"});
+test("reOpen", async({browser}) => {
+  const context = await browser.newContext({ storageState: "./aut.json" });
   const page = await context.newPage();
-    // @ts-ignore
-    await page.goto("http://the-internet.herokuapp.com/secure");  
-    try {
-      await expect(page).toHaveScreenshot("secure.png")
-      console.log('Login successful!');
-    } catch (error) {
-      console.error('Login failed within 2 seconds.');
-      throw error
-    }
-    const currentTime = new Date().toLocaleString();
-    const filePathStored = path.join(process.cwd(), 'time.txt');
-    fs.writeFileSync(filePathStored, currentTime);
 
-    const artifactsPath = process.env.GITHUB_WORKSPACE || process.cwd();
+  await page.goto("http://the-internet.herokuapp.com/secure");
+
+  try {
+    await expect(page).toHaveScreenshot("secure.png");
+
+    // Создаем файл time.txt и записываем в него текущее время
+    const currentTime = new Date().toLocaleString();
+    const filePath = path.join(process.cwd(), 'time.txt');
+    fs.writeFileSync(filePath, currentTime);
+
+    console.log('Login successful!');
+  } catch (error) {
+    console.error('Login failed within 2 seconds.');
+    throw error;
+  }
+});
+
+// Добавляем файлы к Artifacts
+test.afterAll(async() => {
+  const artifactsPath = process.env.GITHUB_WORKSPACE || process.cwd();
   const filePath = path.join(process.cwd(), 'time.txt');
 
   // Перемещаем файл в папку с Artifacts
   fs.renameSync(filePath, path.join(artifactsPath, 'time.txt'));
-
-  })
+});
